1d442b665771bf6ffaeb2f4008a874f221312b08
safhanaila110807-ops <safhanaila110807@gmail.com>
2026-05-22 14:13:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new jpurney")
+st.title("🎈 Our Project Class A")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
